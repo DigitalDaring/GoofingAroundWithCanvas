51bc60481764f5a57daf592e0aeb1879424864ef
Edmund Bates <lutemaster@gmail.com>
2022-04-18 16:08:23 -0700

    dollar explosions

--- falling_dollars.ts
+++ falling_dollars.ts
@@ -2,37 +2,40 @@ import {DollarExplosion} from "./models/dollar-explosion";
 
 const canvas = document.getElementById('dotsCanvas') as HTMLCanvasElement;
 const context = canvas.getContext("2d");
-const dollarExploder = new DollarExplosion();
+const dollarExploder = DollarExplosion.generate(10, 640);
 
 const refreshDollars = () => {
     context.fillStyle = 'rgba(255, 255, 255, 1)';
     context.fillRect(0, 0, 640, 480);
     dollarExploder.dollars.forEach((dollar) => {
         context.fillStyle = `rgba(0, 0, 0, ${dollar.opacity})`;
-        drawCircle(dollar.location.x, dollar.location.y, 4);
-        //drawCircle(dot.location.x, dot.location.y, 4);
+        drawCircle(dollar.location.x, dollar.location.y, 20);
     });
 };
 
 const drawCircle = (x: number, y: number, radius: number) => {
-    context.strokeStyle = 'rgba(0, 0, 0, 1)';
-    context.fillStyle = 'rgba(255, 255, 255, 1)';
+    context.strokeStyle = 'rgba(, 0, 0, 1)';
+    context.fillStyle = 'rgba(0, 146, 216, 1)';
     context.beginPath();
     context.arc(x, y, radius, 0, 2 * Math.PI);
     context.stroke();
     context.fill();
 };
 
-const moveDollarsDown = (speedMultiple) => {
+const moveDollarsDown = (gravity) => {
     dollarExploder.dollars.forEach(dollar => {
         if (dollar.location.y < 1000) {
-            dollar.location.y += dollar.velocity.y * speedMultiple;
+            dollar.location.y += dollar.velocity.y;
+            dollar.location.x += dollar.velocity.x;
+            dollar.velocity.y += gravity;
         }
     });
 };
 
 window.setInterval(() => {
-    moveDollarsDown(.005);
+    moveDollarsDown(.19);
     refreshDollars();
-}, 25);
+}, 10);
 
+
+//#0092D8
--- models/dollar-explosion.ts
+++ models/dollar-explosion.ts
@@ -4,23 +4,26 @@ import {Coordinates} from "./coordinates";
 export class DollarExplosion {
     dollars: DollarBillYall[];
 
-    static generate(amount: number): DollarExplosion {
+    static generate(amount: number, widthOfCanvas: number): DollarExplosion {
 
         let allDollars = [];
         for(let i = 0; i < amount; i++) {
         
             const randomX = Math.floor(Math.random() * 100);
-
+            const isPositiveX = Math.floor(Math.random() * 2) > 0;
+            const randomVelocityX = (isPositiveX ? 1 : -1) * Math.floor(Math.random() * 5);
+            const randomVelocityY = -1 * Math.floor(Math.random() * 20);
+            
             const newCoord = {
-                x: randomX,
-                y: 0
+                x: widthOfCanvas / 2 + randomX,
+                y: 100
             } as Coordinates;
 
             const newDollarBill = {
                 location: newCoord,
                 scale: 1,
                 opacity: 1,
-                velocity: {x: 1, y: 1} as Coordinates
+                velocity: {x: randomVelocityX, y: randomVelocityY} as Coordinates
             } as DollarBillYall;
 
             allDollars = [...allDollars, newDollarBill];
